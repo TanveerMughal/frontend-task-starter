2e106d6422ca8e6acf3383e1d434a7e6008eb3c0
Tanveer Mughal <tanveermughal101@gmail.com>
2024-09-03 15:09:28 +0500

    first commit git config --global user.name "My Name"

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen w-full bg-gay-100">
+      <p className="text-3xl text-gray-600 font-bold">Frontend task template</p>
+    </div>
+  );
+}
+
+export default App;
